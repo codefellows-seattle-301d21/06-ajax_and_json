7f9657a0b43542f0f1e28e8b5a9c02748f562d88
Allan Liebold <allan.liebold@gmail.com>
2017-06-19 16:08:52 -0700

    retrieves data using ajax and caches in localstorage

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -59,5 +59,10 @@ Article.fetchAll = function() {
     // cache it in localStorage so we can skip the server call next time,
     // then load all the data into Article.all with the .loadAll function above,
     // and then render the index page.
+    $.getJSON('/../data/hackerIpsum.json', function(data) {
+      localStorage.rawData = JSON.stringify(data);
+      Article.loadAll(data);
+      articleView.initIndexPage();
+    });
   }
 }
